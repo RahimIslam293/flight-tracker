e3078140b02bd9aa1945688f8df8d947036b37bc
RahimIslam293 <110697424+RahimIslam293@users.noreply.github.com>
2025-05-11 10:37:01 -0400

    Initial Commit and Push

--- data_manager.py
+++ data_manager.py
@@ -8,3 +8,10 @@ class DataManager:
     def __init__(self):
         self.endpoint = "https://api.sheety.co/354c60151bffcfc5fb8b6f35cc5c5dcc/flightDeals/prices"
         self.bearer_token = os.getenv("SHEETY_BEARER_TOKEN")
+        self.headers = {"Authorization": f"Bearer {self.bearer_token}"}
+        self.data_holder = {}
+    def load_data(self):
+        resp = requests.get(url=self.endpoint, headers=self.headers)
+        #resp.raise_for_status()
+        self.data_holder = resp.text
+
--- main.py
+++ main.py
@@ -2,4 +2,14 @@
 import requests
 import dotenv
 import os
+import data_manager as dm
 dotenv.load_dotenv()
+
+d_m = dm.DataManager()
+
+print(d_m.bearer_token)
+print(d_m.headers)
+print(d_m.endpoint)
+d_m.load_data()
+
+print(d_m.data_holder)
